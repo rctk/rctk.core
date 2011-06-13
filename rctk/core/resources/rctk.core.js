8551304c0575b2071418c32b82a2bc24d369479a
Ivo van der Wijk <github@in.m3r.nl>
2011-06-13 20:19:16 +0000

    refactoring/incorporating jquery sharable core code
    git-svn-id: https://rctk.googlecode.com/svn/rctk.core/trunk@566 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctk/core/resources/rctk.core.js
+++ rctk/core/resources/rctk.core.js
@@ -4,6 +4,10 @@ var rctk = rctk || {};
 
 rctk.core = (function($) {
     var not_defined = function(msg) { alert(msg); };
+    var request_count = 0;
+    var poll = false;
+    var interval = 1000;
+    var debug = false;
 
     var handlers = {
         request: function(path, callback, sessionid, data) { 
@@ -11,6 +15,20 @@ rctk.core = (function($) {
         },
         handle: function(task) { 
             not_defined("Handle function not set -> " + task); 
+        },
+        dump: function(data, debug) {
+            // invoked to dump a debug stacktrace
+            alert(data);
+        },
+        busy: function() {
+            // invoked when we're waiting for data
+            // add a delay XXX
+            // setTimeout(function(if request_count...) {}, 1000);
+            $("body").css("cursor", "progress");
+        },
+        idle: function() {
+            // invoked when we're idle / not waiting for data
+            $("body").css("cursor", "auto");
         }
     };
 
@@ -26,9 +44,39 @@ rctk.core = (function($) {
         },
         start: function(sessionid, data) {
             sid = sessionid;
-            if('title' in data) {
-                document.title = data.title;
+            if(jQuery.isArray(data) && 'crash' in data[0] && data[0].crash) {
+              // pass true for debug, since we've never actually received
+              // a configuration
+              handlers.dump(data[0], true);
+              return;
             }
+            if('config' in data) {
+                var config = data.config;
+    
+                if('polling' in config) {
+                    if(config.polling) {
+                        poll = true;
+                        interval = config.polling;
+                        polltimer = setInterval(rctk.util.proxy(this.get_tasks, this), interval)
+                    }
+                    else {
+                        poll = false;
+                    }
+                }
+                if('debug' in config) {
+                    debug = config.debug;
+                }
+                if('title' in config) {
+                      // fails on IE8, argh!
+                      //$("title").html(config.title);
+                      document.title = config.title || '';
+                }
+            }
+            this.get_tasks();
+        },
+        get_tasks: function() {
+            request_count++;
+            handlers.busy();
             handlers.request("pop", rctk.util.proxy(this.handle_tasks, this), sid);
         },
         push: function(task) {
@@ -36,17 +84,22 @@ rctk.core = (function($) {
         },
         flush: function() {
             if(queue.length > 0) {
-                // show_throbber()
+                handlers.busy();
                 handlers.request("task", rctk.util.proxy(this.handle_tasks, this), sid, "queue="+JSON.stringify(queue));
                 queue = [];
             }
         },
         handle_tasks: function(sessionid, data) {
+            request_count--;
+            if(request_count == 0) {
+                handlers.idle();
+            }
             if(data) {
                 for(var i=0; i < data.length; i++) {
                     handlers.handle(data[i]);
                 }
             }
+            
             this.flush(); 
         }
 
